e6181993cb1a76ee7217e0b7f3c26f9490727d25
Hamed Alemohammad <h.alemohammad@gmail.com>
2025-02-19 04:52:28 +0000

    WIP: generating validated time-series chips

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,12 @@
 import numpy as np
 import stackstac 
+import pandas as pd 
 
 def search_s2_scenes(aoi, date_range, catalog, config):
     """
     Searches for Sentinel-2 scenes within the AOI and date range.
     Adds debugging info to identify missing data issues.
     """
-    # catalog = pystac_client.Client.open(
-    #     "https://planetarycomputer.microsoft.com/api/stac/v1",
-    #     modifier=planetary_computer.sign_inplace,
-    # )
     s2_search = catalog.search(
         collections=config["sentinel_2"]["collection"],
         bbox=aoi['geometry'].bounds, 
@@ -23,10 +20,6 @@ def search_s2_scenes(aoi, date_range, catalog, config):
 
 
 def search_lc_scene(bbox, catalog, config):
-    # catalog = pystac_client.Client.open(
-    #     "https://planetarycomputer.microsoft.com/api/stac/v1",
-    #     modifier=planetary_computer.sign_inplace,
-    # )
     lc_search = catalog.search(
         collections=config["land_cover"]["collection"],
         bbox=bbox,
@@ -36,23 +29,23 @@ def search_lc_scene(bbox, catalog, config):
     return items
 
 def stack_s2_data(s2_items, config):
-    print("\nChecking available assets in Sentinel-2 items...")
-    valid_bands = [band for band in config["sentinel_2"]["bands"] if all(band in item.assets for item in s2_items)]
+    try:
+        epsg = s2_items[0].properties["proj:epsg"]
+    except:
+        epsg = int(s2_items[0].properties["proj:code"].split(":")[-1])
+        
     try:
         s2_stack = stackstac.stack(
             s2_items,
-            assets=valid_bands,
-            # epsg=s2_items[0].properties["proj:epsg"],
-            epsg=int(s2_items[0].properties["proj:code"].split(":")[-1]),
+            assets=config["sentinel_2"]["bands"],
+            epsg=epsg,
             resolution=config["sentinel_2"]["resolution"],
             fill_value=np.nan,
             bounds_latlon = s2_items[0].bbox
         )
         # s2_stack_resampled = s2_stack.median("time", skipna=True)
-        s2_stack = s2_stack.chunk(chunks={"band": len(valid_bands), "x": -1, "y": "auto"})
-        
-        print(f"Stacked Sentinel-2 bands: {list(s2_stack.coords['band'].values)}")
-        print(f"Number of time steps: {len(s2_stack.time)}")
+        s2_stack = s2_stack.chunk(chunks={"band": len(config["sentinel_2"]["bands"]), "x": -1, "y": "auto"})
+
         return s2_stack
     except Exception as e:
         print(f"Error stacking Sentinel-2 data: {e}")
@@ -64,7 +57,6 @@ def stack_lc_data(lc_items, s2_epsg, s2_bbox, config):
         print("No Land Cover data found.")
         return None
     try:
-        print("Stacking Land Cover images...")
         stacked_data = stackstac.stack(
             lc_items,
             dtype=np.ubyte,
@@ -73,10 +65,8 @@ def stack_lc_data(lc_items, s2_epsg, s2_bbox, config):
             epsg=s2_epsg,
             resolution=config["sentinel_2"]["resolution"],
             bounds_latlon=s2_bbox,
-        ).squeeze()
+        ).median("time", skipna=True).squeeze()
         stacked_data = stacked_data.chunk(chunks={"x": -1, "y": "auto"})
-        print("Stacked LC data shape:", stacked_data.shape)
-        #print(f"Chunk sizes: {stacked_data.chunks}")   # Uncomment for big chunk size 
         return stacked_data
         
     except Exception as e:
@@ -86,19 +76,23 @@ def stack_lc_data(lc_items, s2_epsg, s2_bbox, config):
 def unique_class(window, axis=None, **kwargs):
     return np.all(window == window[0, 0], axis=axis)
 
-def missing_values(array):
-    """Check if the given Dask DataArray contains NaN values and print only when necessary."""
-    has_nan = array.isnull().any().compute()
-    if has_nan:
-        print("Warning: Missing values detected in the chip!")
+def missing_values(array, chip_size, sample_size):
+    """Check if the given S2/LC stacked array contains NaN values over the cenrtal sample area."""
+    array_trimmed = array.isel(x = slice(int((chip_size - sample_size) / 2), int((chip_size + sample_size) / 2)), 
+                               y = slice(int((chip_size - sample_size) / 2), int((chip_size + sample_size) / 2))
+                              )
+    has_nan = array_trimmed.isnull().any()
     return has_nan
+    
     
 def gen_chips(s2_array, lc_array, index):
 
     lc_path = f"/home/benchuser/data/lc_{index:05}.tif"
-    s2_path = f"/home/benchuser/data/s2_{index:05}.tif"
     try:
-        s2_array.rio.to_raster(s2_path)
+        for dt in s2_array.time.values:
+            ts = pd.to_datetime(str(dt)) 
+            s2_path = f"/home/benchuser/data/s2_{index:05}_{ts.strftime('%Y%m%d')}.tif"
+            s2_array.sel(time = dt).squeeze().rio.to_raster(s2_path)
         lc_array.rio.to_raster(lc_path)
         gen_status = True
     except:
