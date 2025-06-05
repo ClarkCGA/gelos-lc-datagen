9f003da822fba2b57f5f81bbe0e49e36d79507ae
Rufai Omowunmi Balogun <rufaibalogun1@gmail.com>
2025-06-05 22:43:33 +0000

    .gitignore

--- src/utils.py
+++ src/utils.py
@@ -11,12 +11,22 @@ def search_s2_scenes(aoi, date_range, catalog, config):
         collections=config["sentinel_2"]["collection"],
         bbox=aoi['geometry'].bounds, 
         datetime=date_range,
-        query={"eo:cloud_cover": {"lt": config["sentinel_2"]["cloud_cover"]}},
+        query = [f"s2:nodata_pixel_percentage<{config["sentinel_2"]["nodata_pixel_percentage"]}",
+                 f"eo:cloud_cover<{config["sentinel_2"]["cloud_cover"]}"
+                ],
         sortby=["+properties.eo:cloud_cover"],
         max_items=1,
     )
     items = s2_search.item_collection()
     return items
+
+
+def mask_cloudy_pixels(s2_stack):
+    scl = s2_stack.sel(band="SCL")
+    cloud_mask = scl.isin([3, 8, 9, 10])
+    s2_stack = s2_stack.where(~cloud_mask)
+    
+    return s2_stack
 
 
 def search_lc_scene(bbox, catalog, config):
@@ -43,26 +53,24 @@ def stack_s2_data(s2_items, config):
             fill_value=np.nan,
             bounds_latlon = s2_items[0].bbox
         )
-        # s2_stack_resampled = s2_stack.median("time", skipna=True)
         s2_stack = s2_stack.chunk(chunks={"band": len(config["sentinel_2"]["bands"]), "x": -1, "y": "auto"})
-
+        s2_stack = mask_cloudy_pixels(s2_stack)
+        s2_stack = s2_stack.drop_sel(band="SCL")
+        
         return s2_stack
     except Exception as e:
         print(f"Error stacking Sentinel-2 data: {e}")
         return None
 
 
-def stack_lc_data(lc_items, s2_epsg, s2_bbox, config):
+def stack_lc_data(lc_items, epsg, s2_bbox, config):
     if not lc_items:
         print("No Land Cover data found.")
         return None
     try:
         stacked_data = stackstac.stack(
             lc_items,
-            dtype=np.ubyte,
-            fill_value=255,
-            sortby_date=False,
-            epsg=s2_epsg,
+            epsg=epsg,
             resolution=config["sentinel_2"]["resolution"],
             bounds_latlon=s2_bbox,
         ).median("time", skipna=True).squeeze()
@@ -87,16 +95,28 @@ def missing_values(array, chip_size, sample_size):
     
 def gen_chips(s2_array, lc_array, index):
 
-    lc_path = f"/home/benchuser/data/lc_{index:05}.tif"
+    root_path = "/home/benchuser/data/"
+    lc_path = f"{root_path}/lc_{index:06}.tif"
+    dts = []
     try:
         for dt in s2_array.time.values:
             ts = pd.to_datetime(str(dt)) 
-            s2_path = f"/home/benchuser/data/s2_{index:05}_{ts.strftime('%Y%m%d')}.tif"
+            s2_path = f"{root_path}/s2_{index:06}_{ts.strftime('%Y%m%d')}.tif"
             s2_array.sel(time = dt).squeeze().rio.to_raster(s2_path)
+            dts.append(ts.strftime('%Y%m%d'))
         lc_array.rio.to_raster(lc_path)
         gen_status = True
     except:
         gen_status = False
 
 
-    return gen_status
+    return gen_status, dts
+
+def get_continent(point, continents_path):
+    """Returns the continent name for a given shapely Point (in (lon, lat) order)."""
+    continents = gpd.read_file(continents_path)
+
+    for _, row in continents.iterrows():
+        if row['geometry'].contains(point):
+            return row['CONTINENT']
+    return "Unknown"
