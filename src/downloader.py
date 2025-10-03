0ac3f6b8ccb826c9fe05c278d9aa0e8791e05363
Rufai Omowunmi Balogun <rufaibalogun1@gmail.com>
2025-10-03 03:05:08 -0400

    cache burn mask and chip slice computation w/  aoi_processor

--- src/downloader.py
+++ src/downloader.py
@@ -94,65 +94,7 @@ class Downloader:
             self.aoi_path = self.working_directory / 'aoi_metadata.geojson'
             self.chip_metadata_path = self.working_directory / 'chip_metadata.csv'
 
-        # handle the case where the script is starting a new download operation
-        else:
-            # aoi_path = (f'/home/benchuser/code/data/map_{self.config.aoi.version}.geojson')
-            aoi_path = (self.working_directory / f'/map_{self.config.aoi.version}.geojson')
-            self.aoi_gdf = gpd.read_file(aoi_path)
-            if self.config.aoi.exclude_indices:
-                self.aoi_gdf = self.aoi_gdf.drop(self.config.aoi.exclude_indices)
-            if self.config.aoi.include_indices:
-                self.aoi_gdf = self.aoi_gdf.loc[self.config.aoi.include_indices]
-            self.aoi_gdf['status'] = 'not processed'
-            self.aoi_gdf.to_file(self.working_directory / 'aoi_metadata.geojson', driver = 'GeoJSON')
-            self.aoi_processing_gdf = self.aoi_gdf
-            self.chip_metadata_df = pd.DataFrame(columns=[
-                        'chip_index',
-                        'aoi_index',
-                        'sentinel_2_dates',
-                        'sentinel_1_dates',
-                        'landsat_dates',
-                        'land_cover',
-                        'chip_footprint',
-                        'epsg',
-                        'status',
-                ])
-            self.chip_index = 0
-            self.aoi_path = self.working_directory / 'aoi_metadata.geojson'
-            self.chip_metadata_path = self.working_directory / 'chip_metadata.csv'
-    
     def download(self):
-        """Route based on dataset type."""
-        if getattr(self.config, "dataset", None) and getattr(self.config.dataset, "fire", False):
-            return self.download_fire()
-        else:
-            return self.download_lc_chips()
-    
-    def download_lc_chips(self):
-        """Download data for all AOIs that have not yet been processed from the AOI GeoJSON file"""
-        for aoi_index, aoi in self.aoi_processing_gdf.iterrows():
-            aoi_processor = AOI_Processor(
-                aoi_index,
-                aoi,
-                self.chip_index,
-                self.working_directory,
-                self.catalog,
-                self.config,
-            )
-            try:
-                aoi_chip_df = aoi_processor.process_aoi()
-                self.chip_metadata_df = pd.concat([self.chip_metadata_df, aoi_chip_df], ignore_index=True)
-                self.chip_index += len(aoi_chip_df)
-                aoi_status = 'success'
-            except Exception as e:
-                print(e)
-                aoi_status = str(e)
-            finally:
-                self.aoi_gdf.loc[aoi_index, 'status'] = aoi_status
-                self.aoi_gdf.to_file(self.aoi_path, driver='GeoJSON')
-                self.chip_metadata_df.to_csv(self.chip_metadata_path, index=False)
-
-    def download_fire(self):
         for aoi_index, aoi in self.aoi_processing_gdf.iterrows():
             event_date_ranges, control_date_ranges = get_fire_date_ranges(
                                                         aoi, 
@@ -161,6 +103,7 @@ class Downloader:
                                                         "n_control_years", 
                                                         7)
                                                     )
+            
             aoi_processor = AOI_Processor(
                 aoi_index=aoi_index,
                 aoi=aoi,
@@ -169,15 +112,22 @@ class Downloader:
                 catalog=self.catalog,
                 config=self.config,
             )
+
+            event_success = False
+
             try:
                 print(f"Processing Event Chips for AOI {aoi_index:02d}")
                 aoi_chip_df = aoi_processor.process_aoi("event", event_date_ranges, self.chip_metadata_df)
                 self.chip_metadata_df = pd.concat([self.chip_metadata_df, aoi_chip_df], ignore_index=True)
                 aoi_status = "success"
+                event_success = True
             except Exception as e:
                 print(f"[event-error] AOI {aoi_index:02d}: {e}")
                 aoi_status = str(e)
             
+            if not event_success:
+                print(f"[skip-controls] AOI {aoi_index:02d}: event failed or produced no chips.")
+                continue
             for ctrl_dates in control_date_ranges:
                 try:
                     print(f"Processing Control Chips for AOI {aoi_index:02d}")
@@ -190,7 +140,7 @@ class Downloader:
             
             self.aoi_gdf.loc[aoi_index, 'status'] = aoi_status
             self.aoi_gdf.to_file(self.aoi_path, driver='GeoJSON')
-            # self.chip_metadata_df.drop_duplicates(
-            #             subset=["chip_index","platform","date"], keep="last", inplace=True
-            #         )
+            self.chip_metadata_df.drop_duplicates(
+                        subset=["chip_index","platform","date"], keep="last", inplace=True
+                    )
             self.chip_metadata_df.to_csv(self.chip_metadata_path, index=False)
